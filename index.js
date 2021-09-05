b96ef0bde26bb19e7751a95de240fae838aa6ea9
Alexei Garban <alexeigarban@Alexeis-MacBook-Pro.local>
2021-09-05 23:08:04 +0200

    hour test

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ const __dirname = path.dirname(__filename);
 
 /* SET HOUR TO RUN FUNCTION */
 const rule = new schedule.RecurrenceRule();
-rule.hour=22
-rule.minute =59;
+rule.hour=6
+rule.minute =5;
 
 
 /* REPEATED FUNCTION */
